91ea152c854e1fcee5a8eecf823c1eaa1ca02623
mariomelek <mariomelek12345@gmail.com>
2025-03-14 22:49:54 +0200

    last check

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>404 - Not Found</h1>
-      <p>Sorry, we couldn't find the page you're looking for.</p>
+      <p>Sorry, we could not find the page you are looking for.</p>
     </div>
   );
 }
